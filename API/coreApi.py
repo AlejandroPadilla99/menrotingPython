82058bddaf011c53d59637755ff6e395bf6bd723
Alejandro Padilla Flores <padillafloresalejandro99@gmail.com>
2022-07-24 21:52:33 -0500

    api core file and endpoints files created

---
+++ API/coreApi.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+import requests
+
+@dataclass
+class Response:
+    status_code = int
+    text = str
+    as_dict = object
+    headers = dict
+    
+
+class coreApi:
+    def get(self, url):
+        response = requests.get(url)
+        return self.__get__responses(response)
+
+    def post(self, url, playload, headers):
+        response = requests.post(url, data=playload, headers=headers)
+        return self.__get__responses(response)
+    
+    def update(self, url, playload, headers):
+        response = requests.post(url, data=playload, headers=headers)
+        return self.__get__responses(response)
+
+    def delete(self, url):
+        response = requests.get(url)
+        return self.__get__responses(response)
+
+    def __get_responses(self,response):
+        status_code = response.status_code
+        text = response.text
+
+        try:
+            as_dict = response.json()
+        except Exception:
+            as_dict {}
+        
+        headers = response.headers
+        
+        return Response (status_code, text, as_dict, headers)
+        
+    
+
